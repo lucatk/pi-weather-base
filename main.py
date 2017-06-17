b49bfcf145aeaf228646bc04c128d6adc7fd59cc
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-17 03:10:55 +0200

    Alte Zeile wird nun überschrieben

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
-from sys import exit
+from sys import stdout
 
 import sensors.BMP085 as BMP085
 
 sensor_bmp180 = BMP085.BMP085(busnum=config["bmp180"]["i2cbus"])
 
 while True:
-    print "".join(["\r", "\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())])
+    print "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure()), "\r"]) ,
+    stdout.flush()
     time.sleep(1)
