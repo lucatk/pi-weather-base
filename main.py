1f75ac713fa9e27b5102f011e241823e391897be
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-17 04:13:18 +0200

    Print-Fix ohne Curses

--- main.py
+++ main.py
@@ -25,17 +25,19 @@ import sensors.DHT22 as DHT22
 sensor_bmp180 = BMP085.BMP085(busnum=config["bmp180"]["i2cbus"])
 sensor_dht22 = DHT22.DHT22(pinnum=config["dht22"]["pinnum"])
 
-scr = curses.initscr()
-curses.noecho()
-curses.cbreak()
+# scr = curses.initscr()
+# curses.noecho()
+# curses.cbreak()
 
-try:
+# try:
     while True:
-        scr.addstr(0, 0, "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())]))
-        scr.addstr(1, 0, "".join(["\t\tTemperature (DHT22): ", str(sensor_dht22.read_temperature()), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%"]))
-        scr.refresh()
+        print "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure()), "\tTemperature (DHT22): ", str(sensor_dht22.read_temperature(), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%\r")]) ,
+        stdout.flush()
+        # scr.addstr(0, 0, "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())]))
+        # scr.addstr(1, 0, "".join(["\t\tTemperature (DHT22): ", str(sensor_dht22.read_temperature()), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%"]))
+        # scr.refresh()
         time.sleep(1)
-finally:
-    curses.echo()
-    curses.nocbreak()
-    curses.endwin()
+# finally:
+#     curses.echo()
+#     curses.nocbreak()
+#     curses.endwin()
