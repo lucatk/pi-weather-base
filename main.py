03e3f79f40d998d41f16cf2e66372b6fea80b2f0
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-17 03:52:37 +0200

    DHT22-Sensor Support hinzugefügt

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 config = {
     "bmp180": {
         "i2cbus": 1
+    },
+    "dht22": {
+        "pinnum": 4
     }
 }
 
@@ -11,15 +14,29 @@ config = {
 
 import sys
 import time
+import curses
 import RPi.GPIO as GPIO
 
 from sys import stdout
 
 import sensors.BMP085 as BMP085
+import sensors.Adafruit_DHT as Adafruit_DHT
 
 sensor_bmp180 = BMP085.BMP085(busnum=config["bmp180"]["i2cbus"])
+sensor_dht22 = Adafruit_DHT.DHT22
 
-while True:
-    print "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure()), "\r"]) ,
-    stdout.flush()
-    time.sleep(1)
+scr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+
+try:
+    while True:
+        scr.addstr(0, 0, "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())]))
+        humidity, temperature = Adafruit_DHT.read_retry(sensor_dht22, config["dht22"]["pinnum"])
+        scr.addstr(1, 0, "".join(["\t\tTemperature (DHT22): ", str(temperature), "\tHumidity (DHT22): ", str(humidity), "%"]))
+        scr.refresh()
+        time.sleep(1)
+finally:
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
