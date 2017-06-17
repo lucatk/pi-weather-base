f8157d082aef68ea7e6fa0ac97ad532504717ef1
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-17 02:17:34 +0200

    Sensor-Basiscode hinzugefügt, BMP180 hinzugefügt, erste Testausgabe des Sensors

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world.")
+####     CONFIG     ####
+
+config = {
+    "bmp180": {
+        "i2cbus": 1
+    }
+}
+
+####   END CONFIG   ####
+
+
+import sys
+import RPi.GPIO as GPIO
+
+from sys import exit
+
+from sensors import bmp180
+
+temperatureSensor = bmp180.BMP180("temp", config["bmp180"])
+print temperatureSensor.getVal()
