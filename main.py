748ae1c0aa4dd14a6cb3830f3385400cc41745f3
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-17 04:15:48 +0200

    Indent fix

--- main.py
+++ main.py
@@ -30,13 +30,13 @@ sensor_dht22 = DHT22.DHT22(pinnum=config["dht22"]["pinnum"])
 # curses.cbreak()
 
 # try:
-    while True:
-        print "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure()), "\tTemperature (DHT22): ", str(sensor_dht22.read_temperature(), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%\r")]) ,
-        stdout.flush()
-        # scr.addstr(0, 0, "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())]))
-        # scr.addstr(1, 0, "".join(["\t\tTemperature (DHT22): ", str(sensor_dht22.read_temperature()), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%"]))
-        # scr.refresh()
-        time.sleep(1)
+while True:
+    print "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure()), "\tTemperature (DHT22): ", str(sensor_dht22.read_temperature(), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%\r")]) ,
+    stdout.flush()
+    # scr.addstr(0, 0, "".join(["\t\tTemperature (BMP180): ", str(sensor_bmp180.read_temperature()), "\tPressure (BMP180): ", str(sensor_bmp180.read_pressure())]))
+    # scr.addstr(1, 0, "".join(["\t\tTemperature (DHT22): ", str(sensor_dht22.read_temperature()), "\tHumidity (DHT22): ", str(sensor_dht22.read_humidity()), "%"]))
+    # scr.refresh()
+    time.sleep(1)
 # finally:
 #     curses.echo()
 #     curses.nocbreak()
