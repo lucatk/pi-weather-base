5c21e0624140c6b29bf3027f30979eacfcd74352
Luca Killmaier <lucakillmaier@lu-co.de>
2017-06-22 20:30:35 +0200

    Implementierung des MCP3008

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ import json
 from sys import stdout
 
 import Adafruit_DHT
-import Adafruit_BMP
+import Adafruit_BMP.BMP085 as BMP085
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 sensor_DHT = Adafruit_DHT.DHT22
 sensor_DHT_PIN = 4
 
-sensor_BMP = Adafruit_BMP.BMP085.BMP085()
+# sensor_BMP = BMP085.BMP085()
+
+SPI_PORT = 0
+SPI_DEVICE = 0
+sensor_MCP3008 = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 zmqContext = zmq.Context()
 socket = zmqContext.socket(zmq.REQ)
@@ -22,10 +28,12 @@ while True:
     data = {
         "dht_humidity": humid,
         "dht_temperature": dht_temp,
-        "bmp_temperature": sensor_BMP.read_temperature(),
-        "bmp_pressure": sensor_BMP.read_pressure(),
-        "bmp_altitude": sensor_BMP.read_altitude(),
-        "bmp_slpressure": sensor_BMP.read_sealevel_pressure()
+        # "bmp_temperature": sensor_BMP.read_temperature(),
+        # "bmp_pressure": sensor_BMP.read_pressure(),
+        # "bmp_altitude": sensor_BMP.read_altitude(),
+        # "bmp_slpressure": sensor_BMP.read_sealevel_pressure(),
+        "mcp_smoke_voltage": sensor_MCP3008.read_adc(0),
+        "mcp_light_voltage": sensor_MCP3008.read_adc(1)
     }
     socket.send_json(json.dumps(data))
     socket.recv()
